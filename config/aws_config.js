f9a8d65f52568c483875b039e0d1737ec750338e
dhruv mishra <dhruv35m@gmail.com>
2023-12-19 17:35:52 +0530

    path updated

--- config/aws_config.js
+++ config/aws_config.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
-const s3fs = require("@cyclic.sh/s3fs/promises")(process.env.S3_BUCKET_NAME);
+const s3fs = require("@cyclic.sh/s3fs/promises")(
+  process.env.CYCLIC_BUCKET_NAME
+);
 
 const initializeS3 = () => {
-  const fs = s3fs(process.env.S3_BUCKET_NAME, {
+  const fs = s3fs(process.env.CYCLIC_BUCKET_NAME, {
     region: process.env.AWS_REGION,
     credentials: {
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
